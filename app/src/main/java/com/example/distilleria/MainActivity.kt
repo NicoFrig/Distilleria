52ed56a386c6965c481d5058da57c5096c6fa969
NicoMartinetti <nicolo.martinetti@edu.itspiemonte.it>
2022-05-04 17:36:01 +0200

    SignIn

--- app/src/main/java/com/example/distilleria/MainActivity.kt
+++ app/src/main/java/com/example/distilleria/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.distilleria
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
