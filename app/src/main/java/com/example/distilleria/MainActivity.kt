bc7e1268bb689dad2e23cc1351b6ef6edff29785
NicoFrig <nicolo.frighi@edu.itspiemonte.it>
2022-05-11 17:44:32 +0200

    Modificato login e register e aggiunto la pagina principale

--- app/src/main/java/com/example/distilleria/MainActivity.kt
+++ app/src/main/java/com/example/distilleria/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.distilleria
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var register = findViewById<TextView>(R.id.register)
+        register.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/distilleria/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.example.distilleria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        var backButton = findViewById<ImageButton>(R.id.backArrow)
+        backButton.setOnClickListener{
+            finish()
+        }
+    }
+}
