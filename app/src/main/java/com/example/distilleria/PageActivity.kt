070c86844ca62abff2a0d72930a9be109b15d809
NicoFrig <nicolo.frighi@edu.itspiemonte.it>
2022-05-17 12:55:51 +0200

    Termitano menu laterale e aggiunto la pgina del negozio

--- app/src/main/java/com/example/distilleria/PageActivity.kt
+++ app/src/main/java/com/example/distilleria/PageActivity.kt
@@ -1,23 +1,36 @@
 package com.example.distilleria
 
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.view.menu.MenuView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class PageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_home_page, HomePageFragment())
-            .commit()
+//        supportFragmentManager.beginTransaction()
+//            .add(R.id.fragmentContainer, HomePageFragment())
+//            .commit()
 
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
         val menu = findViewById<ImageButton>(R.id.imageButton)
+        val menuSide = findViewById<NavigationView>(R.id.menu)
+        val navController = findNavController(R.id.fragmentContainer)
+        menuSide.setupWithNavController(navController)
+
 
         menu.setOnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
