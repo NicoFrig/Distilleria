7e5a2565492b673243079a3f8cbfb4a52da636fd
NicoFrig <nicolo.frighi@edu.itspiemonte.it>
2022-05-27 11:30:56 +0200

    Aggiunto informazioni con i link ai social

---
+++ app/src/main/java/com/example/distilleria/InfoFragment.kt
@@ -0,0 +1,82 @@
+package com.example.distilleria
+
+import android.content.Intent
+import android.media.Image
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InfoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class InfoFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private lateinit var insta: ImageButton
+    private lateinit var face: ImageButton
+    private var uriInsta:Uri = Uri.parse("https://www.instagram.com/antica_distilleria_quaglia/?hl=en")
+    private var uriFace:Uri = Uri.parse("https://www.facebook.com/anticadistilleriaquaglia")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        insta = view.findViewById(R.id.insta)
+        face = view.findViewById(R.id.face)
+
+        insta.setOnClickListener{
+            var intent = Intent(Intent.ACTION_VIEW, uriInsta)
+            startActivity(intent)
+        }
+        face.setOnClickListener{
+            var intent = Intent(Intent.ACTION_VIEW, uriFace)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment InfoFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            InfoFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
