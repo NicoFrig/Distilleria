436a75e2aaffda737ea0c9ef79b963188be6a141
Pippo <nicolo.frighi@edu.itspiemonte.it>
2022-06-08 17:48:49 +0200

    Inizio recycler view

--- app/src/main/java/com/example/distilleria/DetailFragment.kt
+++ app/src/main/java/com/example/distilleria/DetailFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.NavHostFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,7 @@ class DetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var back:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +38,14 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        back=view.findViewById(R.id.backArrow)
+        back.setOnClickListener{
+            NavHostFragment.findNavController(this).navigate(R.id.action_detailFragment_to_shopFragment)
+        }
     }
 
 }
