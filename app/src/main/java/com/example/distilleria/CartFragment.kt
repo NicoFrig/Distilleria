0126939c23ca3304f00126ac1a4edaad2759d60b
NicoMartinetti <nicolo.martinetti@edu.itspiemonte.it>
2022-06-08 17:53:31 +0200

    CartFragment

--- app/src/main/java/com/example/distilleria/CartFragment.kt
+++ app/src/main/java/com/example/distilleria/CartFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.navigation.fragment.NavHostFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,15 +13,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ShopFragment.newInstance] factory method to
+ * Use the [CartFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ShopFragment : Fragment() {
+class CartFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    private lateinit var prod: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,20 +34,6 @@ class ShopFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        prod = view.findViewById(R.id.product1)
-
-        prod.setOnClickListener{
-//              val frag=activity?.supportFragmentManager?.beginTransaction()
-//              frag?.replace(R.id.fragmentContainer, DetailFragment())
-//              frag?.disallowAddToBackStack()
-//              frag?.commit(
-
-            NavHostFragment.findNavController(this).navigate(R.id.action_shopFragment_to_detailFragment)
-        }
+        return inflater.inflate(R.layout.fragment_cart, container, false)
     }
 }
